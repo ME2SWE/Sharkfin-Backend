713e0337ec12aa415ae5c25ed8ae23cf1ff5ddec
Howard Yoon <howardhyoon@gmail.com>
2023-03-28 17:56:45 -0700

    Query fixes

--- database/schema.sql
+++ database/schema.sql
@@ -73,6 +73,7 @@ CREATE TABLE IF NOT EXISTS finances (
 -- EXAMPLE INSERT STATEMENT: INSERT INTO finances (user_id, transaction_type, amount, avail_balance) VALUES (1, 'bank', 1000, COALESCE((SELECT avail_balance FROM finances WHERE id = (SELECT MAX(id) FROM finances)), 0) + 1000);
 insert into finances ("user_id","transaction_type","amount","net_deposits","avail_balance") values (1,'bank',0,1000,1000);
 insert into finances ("user_id","transaction_type","amount","net_deposits","avail_balance") values (1,'bank',1000,2000,2000);
+insert into finances ("user_id","transaction_type","amount","net_deposits","avail_balance") values (3,'bank',0,500,500);
 
 CREATE TABLE IF NOT EXISTS performance (
   id SERIAL PRIMARY KEY NOT NULL,
