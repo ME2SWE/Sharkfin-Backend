baf2d638ab22dc44ba0844fafae1c369f36ce1b7
Jacinthe Chong <raghavjacinthe@gmail.com>
2023-03-31 12:22:51 -0400

    refactored chat queries to get chats from user and error handling

--- database/schema.sql
+++ database/schema.sql
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS transactions (
   type trade_type,
   datetime TEXT NOT NULL,
   stock_ticker TEXT NOT NULL,
-  quantity integer NOT NULL,
+  quantity NUMBER NOT NULL,
   price TEXT NOT NULL,
   status status_type
 );
